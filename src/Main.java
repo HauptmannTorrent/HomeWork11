ce2aece679bd6b187cf1270c45371d8d02faa91b
Admin <shtern.94@mail.ru>
2022-09-05 21:29:21 +1000

    Второй коммит

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+
 public class Main {
 
-    // Задание 1. Типы переменных
+    public static void main(String[] args) {
+
+    // Задание 1.
+
+    // Типы переменных:
     // Целочисленные
     byte a = 121;
     short b = 30000;
@@ -14,4 +22,32 @@ public class Main {
     // Логические
     boolean h = a > 110;
 
+    // Задание 2.
+
+    double boxer1 = 78.2;
+    double boxer2 = 82.7;
+    double weightOfAllBoxer = boxer1 + boxer2;
+    double weightDifference = boxer2 - boxer1;
+    System.out.println("Общий вес боксёров " + weightOfAllBoxer);
+    System.out.println("Разница в весе боксёров " + weightDifference);
+
+    // Задание 3.
+
+    byte bananas = 5;
+    short bananasWeight = (short) (bananas * 80);
+    short milk = 2;
+    short milkWeight = (short) (milk * 105);
+    short iceCream = 2;
+    short iceCreamWeight = (short) (iceCream * 100);
+    byte eggs = 4;
+    short eggsWeight = (short) (eggs * 70);
+    double fullWeight = bananasWeight + milkWeight + iceCreamWeight + eggsWeight;
+    short grPerKg = 1000;
+    double fullWeightInKIlo = fullWeight / grPerKg;
+    System.out.println("Вес завтрака в килограммах = " + fullWeightInKIlo);
+
+    // Задание 4
+    byte excessWeight = 7;
+
+    }
 }
