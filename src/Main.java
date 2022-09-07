118922c5a1b52b6bfd86ab43c141def6af0fcf4f
Admin <shtern.94@mail.ru>
2022-09-07 10:10:12 +1000

    Третий коммит

--- src/Main.java
+++ src/Main.java
@@ -46,8 +46,43 @@ public class Main {
     double fullWeightInKIlo = fullWeight / grPerKg;
     System.out.println("Вес завтрака в килограммах = " + fullWeightInKIlo);
 
-    // Задание 4
-    byte excessWeight = 7;
+    // Задание 4.
 
+    byte excessWeight = 7;
+    short lW1 = 250;
+    short lW2 = 500;
+    short grammPerKg = 1000;
+    short excessWeightPerGramm = (short) (excessWeight * grammPerKg);
+    short time1 = (short) (excessWeightPerGramm / lW1);
+    short time2 = (short) (excessWeightPerGramm / lW2);
+    System.out.println("Если спортсмен теряет 250 грамм каждый день то ему нужно "+ time1+ " дней.");
+    System.out.println("Если спортсмен теряет 500 грамм каждый день то ему нужно "+ time2+ " дней.");
+
+    // Задание 5.
+
+    double mSalary = 67760;
+    short mSalaryA = (short) (mSalary / 100 * 10);
+    int mSalaryPerYear = (int) (mSalary * 12);
+    int mSalaryAPerMonth = (int) (mSalary + mSalaryA);
+    int mSalaryAPerYear = mSalaryAPerMonth * 12;
+    int mSalDif = mSalaryAPerYear - mSalaryPerYear;
+
+    double dSalary = 83690;
+    short dSalaryA = (short) (dSalary / 100 * 10);
+    int dSalaryPerYear = (int) (dSalary * 12);
+    int dSalaryAPerMonth = (int) (dSalary + dSalaryA);
+    int dSalaryAPerYear = dSalaryAPerMonth * 12;
+    int dSalDif = dSalaryAPerYear - dSalaryPerYear;
+
+    double kSalary = 76230;
+    short kSalaryA = (short) (kSalary / 100 * 10);
+    int kSalaryPerYear = (int) (kSalary * 12);
+    int kSalaryAPerMonth = (int) (kSalary + kSalaryA);
+    int kSalaryAPerYear = kSalaryAPerMonth * 12;
+    int kSalDif = kSalaryAPerYear - kSalaryPerYear;
+
+    System.out.println("Маша теперь получает " + mSalaryAPerMonth + " рублей. Годовой доход вырос на " + mSalDif + " рублей.");
+    System.out.println("Денис теперь получает " + dSalaryAPerMonth + " рублей. Годовой доход вырос на " + dSalDif + " рублей.");
+    System.out.println("Кристина теперь получает " + kSalaryAPerMonth + " рублей. Годовой доход вырос на " + kSalDif + " рублей.");
     }
 }
